df4ec29fe263350ea86a88318cebf2fd6e9698bd
omm-chandra <109668287+omm-chandra@users.noreply.github.com>
2023-03-06 10:33:23 +0530

    second commit

---
+++ src/API/fetchApi.js
@@ -0,0 +1,44 @@
+import Nike from "../image/nike.png"
+import adiddas from "../image/adiddas.png"
+import airmax from "../image/airmax.png"
+
+
+const fetchUrl = [
+    {
+        id:0,
+        shoeName:"Nike",
+        imageUrl:{Nike},
+        price:"$120"
+    },
+    {
+        id:1,
+        shoeName:"AirJordan",
+        imageUrl:{Nike},
+        price:"$160"
+    },
+    {
+        id:2,
+        shoeName:"Adiddas",
+        imageUrl:{adiddas},
+        price:"$140"
+    },
+    {
+        id:3,
+        shoeName:"Gucci",
+        imageUrl:{airmax},
+        price:"$220"
+    },
+    {
+        id:4,
+        shoeName:"Airmax",
+        imageUrl:{adiddas},
+        price:"$150"
+    },{
+        id:5,
+        shoeName:"AirJordan",
+        imageUrl:{Nike},
+        price:"$180"
+    }
+]
+
+export default fetchUrl;
---
+++ src/HOME/home.js
@@ -0,0 +1,72 @@
+import Nike from "../image/nike.png"
+import adiddas from "../image/adiddas.png"
+import airmax from "../image/airmax.png"
+import airjordan from "../image/airjordan.png"
+import "./home.css"
+
+const Home = () => {
+
+    // const 
+
+    return (
+        <>
+            <div id="background">
+                <div id="center_div">
+                    <div id="nav_bar">
+                        <ul>
+                            <li>MEN</li>
+                            <li>WOMEN</li>
+                            <li>SALE</li>
+                            <li>SNKRS</li>
+                        </ul>
+                    </div>
+                    <hr/>
+                    <div id="hero_box">
+                        <div id="left_box">
+                            <h1 id="main">Nike Air Max</h1><br/>
+                            <h1 id="sec_main">Lebagh</h1>
+                            <p>Nike Air Max is a line of shoes produced by Nike,
+                                Air Max shoes are identified by their midsoles incorporating flexible urethane pouches filled
+                                with pressurized gas,
+                                visible from the exterior of the shoe and intended to provide cushioning to the underfoot.</p>
+                            <div id="tags">
+                                <h1>$190</h1>
+                                <button>Add to cart</button>
+                            </div>
+
+                            {/* show slider create here  */}
+                            <div id="slider_div">
+                                <div id="box">
+                                    <img src={adiddas} alt="adiddas" />
+                                    <div>
+                                        <h4>Nike</h4>
+                                        <h3>$180</h3>
+                                    </div>
+                                </div>
+                                <div id="box">
+                                    <img src={airjordan} alt="adiddas" />
+                                    <div>
+                                        <h4>Adiddas</h4>
+                                        <h3>$120</h3>
+                                    </div>
+                                </div>
+                                <div id="box">
+                                    <img src={airmax} alt="adiddas" />
+                                    <div>
+                                        <h4>AirJordan</h4>
+                                        <h3>$220</h3>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                            <img src={Nike} alt="Nike" id="big_shoe"/>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;
